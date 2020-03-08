62fd78fbdaab66ae51d612ab04940827fba960cf
Soufiane ounida <soufianeounidainfo@gmail.com>
2020-03-08 14:11:55 +0100

    Creation project chart + 2 components dashboard + charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ChartsComponent } from './components/charts/charts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ChartsComponent
   ],
   imports: [
     BrowserModule,
